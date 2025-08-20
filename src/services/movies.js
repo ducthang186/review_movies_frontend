19f794b507c60938d70054d63cc989191bcf7594
Student <Student@gmai.com>
2025-08-20 20:33:01 +0700

    feat: first touch

---
+++ src/services/movies.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+class MovieDataService {
+  getAll(page = 0) {
+    return axios.get(`https:/review-movies.onrender.com/movies?page=${page}`);
+    //return axios.get(`https:/review-movies.onrender.com/movies?`, {params: {page: page}})
+  }
+  get(id) {
+    return axios.get(`https:/review-movies.onrender.com/movies/${id}`);
+  }
+  find(query, by = "title", page = 0) {
+    return axios.get(
+      `https:/review-movies.onrender.com/movies?${by}=${query}&page=${page}`
+    );
+  }
+  createReview(data) {
+    return axios.post("https:/review-movies.onrender.com/reviews", data);
+  }
+  updateReview(data) {
+    return axios.put("https:/review-movies.onrender.com/reviews", data);
+  }
+  deleteReview(data) {
+    return axios.delete("https:/review-movies.onrender.com/reviews", data);
+  }
+  getRatings() {
+    return axios.get("https:/review-movies.onrender.com/movies/ratings");
+  }
+}
+const movieDataServiceInstance = new MovieDataService();
+
+export default movieDataServiceInstance;
